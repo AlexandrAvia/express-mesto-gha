dcf4f432f140f0a23ea60d30975f30cb1718db85
Alexandr Semenov <sancho220394@gmail.com>
2022-06-25 23:25:10 +0300

    на ревью

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.use((req, res, next) => {
 
 app.use(userRoutes);
 app.use(cardRoutes);
-app.use((req, res) => {
-  res.status(404).send("Sorry can't find that!");
+app.use((_, res) => {
+  res.status(404).send({ message: 'Несуществующая страница' });
 });
 
 app.listen(PORT);
